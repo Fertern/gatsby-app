93142d4838eb19b416e615f28093973ee9dc72be
Fertern <ferternever@gmail.com>
2020-02-11 15:45:35 +0200

    jsx format, test routing

--- src/pages/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-const app = () => {
-  return <div>test some text</div>
-}
-
-export default app
---
+++ src/pages/index.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const app = () => {
+  return(
+    <div className="wrapper">
+      <Link to='test'>Go to test page</Link>
+    </div>
+  )
+}
+
+export default app
